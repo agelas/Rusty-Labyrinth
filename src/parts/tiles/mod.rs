7287cf06ddde48ee8d890700253656de97f6aa9f
Mathias <mathiaswi177@gmail.com>
2022-11-28 22:10:58 -0800

    Implementations for Tile interface

--- src/parts/tiles/mod.rs
+++ src/parts/tiles/mod.rs
@@ -4,10 +4,56 @@ pub mod tiles {
         BLOCK,
     }
 
-    pub trait Tile { // let me just say I'm so confused if this is how "interfaces" work
+    pub trait Tile { // The 'interface' so to speak
         fn check_move_onto(&self, entity: Entity, fromPosition: Position, tilePosition: Position) -> MoveResult;
         fn is_goal(&self) -> bool;
         fn get_glyph(&self) -> String;
     }    
+
+    pub struct Wall {}
+    pub struct Floor {}
+    pub struct Goal {}
+
+    impl Tile for Wall {
+        fn check_move_onto(&self, entity: Entity, fromPosition: Position, tilePosition: Position) -> MoveResult {
+            MoveResult::BLOCK
+        }
+
+        fn is_goal(&self) -> bool {
+            false
+        }
+
+        fn get_glyph(&self) -> String {
+            std::String('#');
+        }
+    }
+
+    impl Tile for Floor {
+        fn check_move_onto(&self, entity: Entity, fromPosition: Position, tilePosition: Position) -> MoveResult {
+            MoveResult::ALLOW
+        }
+
+        fn is_goal(&self) -> bool {
+            false
+        }
+
+        fn get_glyph(&self) -> String {
+            std::String('.');
+        }
+    }
+
+    impl Goal for Floor {
+        fn check_move_onto(&self, entity: Entity, fromPosition: Position, tilePosition: Position) -> MoveResult {
+            MoveResult::ALLOW
+        }
+
+        fn is_goal(&self) -> bool {
+            true
+        }
+
+        fn get_glyph(&self) -> String {
+            std::String('<');
+        }
+    }
 
 }
